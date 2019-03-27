c7068957892ed4bda0f931df8ddf6c52e4cb332c
tawhidabdullah <tawhidabdullah22@gmail.com>
2019-03-27 18:19:57 +0600

    initial commit

---
+++ jsTalk/function/code.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+// check if an function is exist or not 
+
+function coolFunction (){
+
+}
+
+
+if ('function' === typeof global.coolFunction) {
+    // use coolFunctoin()
+  } else {
+    // do something else
+    console.log('get a life')
+  }
+
+
